f2f8ddecd4cbe777ae9385c93d53f3d430b0e8e9
Aaron Wilson <aaron.wilson@it.ox.ac.uk>
2015-05-07 09:02:01 +0100

    fix wsgi script shipped with munkiwebadmin for 1.8

--- wsgi.py
+++ wsgi.py
@@ -14,12 +14,38 @@ framework.
 
 """
 import os
+import site
+import sys
+
+
+# Remember original sys.path.
+prev_sys_path = list(sys.path)
+
+# we add currently directory to path and change to it
+pwd    = os.path.dirname(os.path.abspath(__file__))
+venvwd = os.path.dirname(pwd)
+
+sys.path = [venvwd, pwd ] + sys.path
+os.chdir(venvwd)
+# find the site-packages within the local virtualenv
+for python_dir in os.listdir('lib'):
+    site_packages_dir = os.path.join('lib', python_dir, 'site-packages')
+    if os.path.exists(site_packages_dir):
+        site.addsitedir(os.path.abspath(site_packages_dir))
+
+new_sys_path = []
+for item in list(sys.path):
+    if item not in prev_sys_path:
+        new_sys_path.append(item)
+        sys.path.remove(item)
+sys.path[:0] = new_sys_path
 
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = "munkiwebadmin.settings"
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "munkiwebadmin.settings")
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "munkiwebadmin.settings")
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
@@ -27,6 +53,3 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "munkiwebadmin.settings")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
-# Apply WSGI middleware here.
-# from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
